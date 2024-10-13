cf89b52308042322d2517b26e2a47642459479d9
Jonathan Forrider <jd4rider@gmail.com>
2024-10-13 13:35:22 -0600

    Newer 'fmt' way

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private TMP_Text highScoreText;
     
     void Start() {
-        highScoreText.text = "High Score: " + PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
+        highScoreText.text = $"High Score: {PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0)}";
     }
     public void Play() {
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
